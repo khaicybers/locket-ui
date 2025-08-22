ee49a0306444596077a2c39d68d552069385644b
khaicybers <khaicybers@gmail.com>
2025-08-23 01:45:32 +0700

    Ensure vite-plugin-pwa

--- src/layouts/locketLayout.jsx
+++ src/layouts/locketLayout.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "@/components/Sidebar";
-import { ContactSupportButton } from "@/components/ui/ContactSupportButton";
+// import { ContactSupportButton } from "@/components/ui/ContactSupportButton"; // Tạm comment
 import { useFeatureVisible } from "@/hooks/useFeature";
 import React from "react";
 
@@ -9,7 +9,7 @@ const LocketLayout = ({ children }) => {
     <div className="overflow-hidden grid grid-rows-[auto_1fr_auto] bg-base-100 text-base-content">
       <main className="overflow-hidden">{children}</main>
       <Sidebar />
-      {!isButtonSupportVisible && <ContactSupportButton />}
+      {/* {!isButtonSupportVisible && <ContactSupportButton />} */}
     </div>
   );
 };
